231f8b230dff75a1b1babae3681a1453cf359e1b
Chris Herwig <chris@mapbox.com>
2012-11-17 20:27:33 -0500

    update with POIs

--- site.js
+++ site.js
@@ -2,20 +2,14 @@ function mmg_google_docs(id, callback) {
 	if (typeof reqwest === 'undefined') {
 		throw 'CSV: reqwest required for mmg_csv_url';
 	}
-
 	function response(x) {
+		console.log(x);
 		var features = [],
 			latfield = '',
 			lonfield = '';
-		x = $.parseJSON(x);
 		if (!x || !x.data) return features;
 
-		for (var f in x.data[0]) {
-			if (f.match(/\$Lat/i)) latfield = f;
-			if (f.match(/\$Lon/i)) lonfield = f;
-		}
-
-		for (var i = 0; i < x.data.length; i++) {
+		for (var i = 0; i < x.data.length; i++) {			
 			var entry = x.data[i];
 			var symbol = "star-stroked";
 			var mc = "#333";
@@ -42,33 +36,27 @@ function mmg_google_docs(id, callback) {
 			case "rail":
 				symbol = "bus";
 				break;
-			}
+		}
 			var feature = {
 				geometry: {
 					type: 'Point',
-					coordinates: []
+					coordinates: [ parseFloat(entry['Longitude']), parseFloat(entry['Latitude']) ]
 				},
 				properties: {
 					"marker-color": mc,
 					"marker-size": "small",
 					"marker-symbol": symbol,
-					"title": '<h3>' + entry['title'] + '</h3>',
-					"description": '<p>' + entry['description'] + '</p>'
+					"title": "<h3 class='map-title'>" + entry['Title'] + "</h3>",
+					"description": "<p>" + entry['Description'] + "</p>"
 				}
-			};
-			for (var y in entry) {
-				if (y === latfield) feature.geometry.coordinates[1] = parseFloat(entry[y].$t);
-				else if (y === lonfield) feature.geometry.coordinates[0] = parseFloat(entry[y].$t);
-				else if (y.indexOf('gsx$') === 0) {
-					feature.properties[y.replace('gsx$', '')] = entry[y].$t;
-				}
-			}
-			if (feature.geometry.coordinates.length == 2) features.push(feature);
+			};			
+			// if (feature.geometry.coordinates.length == 2) 
+			features.push(feature);
 		}
 
 		return callback(features);
+		console.log(features);
 	}
-	//	var url = 'https://fusiontables.googleusercontent.com/fusiontables/api/query?sql=SELECT+*+FROM+'+ id + '&alt=jsonp&typed=true&jsonCallback=callback';
 	var url = 'http://ft2json.appspot.com/q?sql=SELECT%20*%20FROM%20' + id + '&limit=150&jsonp=callback';
 	$.ajax({
 		url: url,
@@ -92,7 +80,8 @@ if (window.location.href.split('#')[1].replace(/\//g, "").replace(/\./g, "") == 
 
 mmg_google_docs('13OpCFyJDjWKJMqZt7kJSNmtKBX8WxShIfnbL4KU', function(features) {
 	features = features.map(function(f) {
-		f.properties.title = f.properties.title, f.properties.description = f.properties.description;
+		f.properties.title = f.properties.title,
+		f.properties.description = f.properties.description;
 		return f;
 	});
 	var markerLayer = mapbox.markers.layer().features(features);
